209e115766ddbb56e51ea40a37b37344f3f78b9e
Anurag Bhattacharyya <bhattacharyyanurag@gmail.com>
2025-03-09 12:50:53 -0700

    first commit

---
+++ build/CMakeFiles/OpenGL_Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL_Project.dir/glad/src/glad.c.o"
+  "CMakeFiles/OpenGL_Project.dir/glad/src/glad.c.o.d"
+  "CMakeFiles/OpenGL_Project.dir/triangle.cpp.o"
+  "CMakeFiles/OpenGL_Project.dir/triangle.cpp.o.d"
+  "OpenGL_Project"
+  "OpenGL_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGL_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
